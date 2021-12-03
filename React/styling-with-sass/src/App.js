0e474edde51296cc98182f00826369f58b186813
ark <39788908+arkhyeon@users.noreply.github.com>
2021-12-03 18:03:37 +0900

    SCSS Practice

--- React/styling-with-sass/src/App.js
+++ React/styling-with-sass/src/App.js
@@ -6,7 +6,57 @@ function App() {
     return (
         <div className="App">
             <div className="buttons">
+                <Button size="large">Button</Button>
                 <Button>Button</Button>
+                <Button size="small">Button</Button>
+            </div>
+            <div className="buttons">
+                <Button size="large" color="gray">
+                    Button
+                </Button>
+                <Button color="gray">Button</Button>
+                <Button size="small" color="gray">
+                    Button
+                </Button>
+            </div>
+            <div className="buttons">
+                <Button size="large" color="pink">
+                    Button
+                </Button>
+                <Button color="pink">Button</Button>
+                <Button size="small" color="pink">
+                    Button
+                </Button>
+            </div>
+            <div className="buttons">
+                <Button outline>Button</Button>
+                <Button outline color="gray">
+                    Button
+                </Button>
+                <Button outline color="pink">
+                    Button
+                </Button>
+            </div>
+            <div className="buttons">
+                <Button outline fullWidth className="custom-button">
+                    Button
+                </Button>
+                <Button outline fullWidth color="gray">
+                    Button
+                </Button>
+                <Button
+                    outline
+                    fullWidth
+                    color="pink"
+                    onClick={() => {
+                        alert("click");
+                    }}
+                    onMouseMove={() => {
+                        console.log("mouse move");
+                    }}
+                >
+                    Button
+                </Button>
             </div>
         </div>
     );
