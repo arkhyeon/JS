97a569169bf26e3fe054feb31985c08e55ea0042
JSH <qkdwn48@naver.com>
2022-04-16 04:09:46 +0900

    [기능] React Router Dom v6

--- state-manage/src/App.jsx
+++ state-manage/src/App.jsx
@@ -1,6 +1,8 @@
 import CounterContainer from './containers/CounterContainer';
 import TodosContainer from './containers/TodosContainer';
 import { css, jsx } from '@emotion/react';
+import Cart from './Page/Cart';
+import Shop from './Page/Shop';
 
 const base = css`
   background-color: darkgreen;
@@ -9,6 +11,8 @@ const base = css`
 function App() {
   return (
     <div css={base}>
+      <Cart></Cart>
+      <Shop></Shop>
       <CounterContainer />
       <hr />
       <TodosContainer />
